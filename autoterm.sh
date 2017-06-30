0367ca54cc2bb2cb33e31f9d5b1343cb1e03d368
Enny Jole <engjole@gmail.com>
2017-06-30 11:53:24 -0700

    Fixing file path.

--- autoterm.sh
+++ autoterm.sh
@@ -3,4 +3,4 @@
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin #environment
 mkdir /home/pi/.config/autostart;
 mv /home/pi/dd_script/terminal.desktop /home/pi/.config/autostart;
-mv /home/pi/dd_script/launch_chrome.sh.desktop /home/pi/.confg/autostart;
+mv /home/pi/dd_script/launch_chrome.sh.desktop /home/pi/.config/autostart;
