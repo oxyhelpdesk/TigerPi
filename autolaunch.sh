ac23590c3c4a9c818ca2321b98249d44f0991e9a
Enny Jole <engjole@gmail.com>
2017-06-29 20:23:51 -0700

    Added slideshow auto-start and wifi auto-connect

---
+++ autolaunch.sh
@@ -0,0 +1,4 @@
+#Script by Enny Jole
+#Autoconnects to wifi and launches slideshow on terminal launch
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin #environment
+echo "/home/pi/dd_script/launch_chrome.sh; /home/pi/dd_script/wifi.sh" >> .bashrc #adds scripts to .bashrc
---
+++ autoterm.sh
@@ -0,0 +1,4 @@
+#Script by Enny Jole
+#Autostarts terminal on reboot
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin #environment
+mv /home/pi/dd_script/terminal.desktop /home/pi/.config/autostart
