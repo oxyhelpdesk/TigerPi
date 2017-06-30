25c60b8e81271425aacd37c50ac21e2964819c38
Enny Jole <engjole@gmail.com>
2017-06-30 12:18:14 -0700

    Final version of autolaunch

--- autoterm.sh
+++ autoterm.sh
@@ -2,5 +2,4 @@
 #Autostarts terminal on reboot
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin #environment
 mkdir /home/pi/.config/autostart;
-mv /home/pi/dd_script/terminal.desktop /home/pi/.config/autostart;
 mv /home/pi/dd_script/launch_chrome.sh.desktop /home/pi/.config/autostart;
--- launch_chrome.sh
+++ launch_chrome.sh
@@ -1,8 +1,8 @@
 #Script by Enny Jole '18, modified from Sam's original code, copied below
 #This script takes a URL surrounded by single quotes placed in a text file and directs Chromium to go to that page
 #The extra flags are needed in order to kill the session restore bubble Chromium displays after an improper shutdown
-sleep 60; #pauses script so wifi connection can be ensured
-file="slideshow.txt"; #variable declaration for text file
+sleep 30; #pauses script so wifi connection can be ensured
+file="/home/pi/dd_script/slideshow.txt"; #variable declaration for text file
 SLIDESHOW=$(cat "$file"); #variable declaration for network
 echo $SLIDESHOW; #initializes variable
 chromium-browser --kiosk --incognito --no-first-run --disable-infobars --disable-session-crashed-bubble $SLIDESHOW; #launches Chrome
