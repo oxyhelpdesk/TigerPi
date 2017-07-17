91cacbfd90c6504bac46b16abdf51450d43e6539
Enny Jole <engjole@gmail.com>
2017-07-17 15:29:49 -0700

    Adding conditional for autoterm script

--- autoterm.sh
+++ autoterm.sh
@@ -1,5 +1,10 @@
 #Script by Enny Jole
 #Autostarts terminal on reboot
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin #environment
-#mkdir /home/pi/.config/autostart;
-mv /home/pi/dd_script/launch_chrome.sh.desktop /home/pi/.config/autostart;
+directory='/home/pi/.config/autostart/'
+if [ -d "$directory" ]; then
+  mv /home/pi/dd_script/launch_chrome.sh.desktop /home/pi/.config/autostart
+else
+  mkdir /home/pi/.config/autostart
+  mv /home/pi/dd_script/launch_chrome.sh.desktop /home/pi/.config/autostart;
+fi
