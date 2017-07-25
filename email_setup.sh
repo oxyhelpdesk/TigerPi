079fc1f9461697aca2c493ebeb6c810189f5995e
Enny Jole <engjole@gmail.com>
2017-07-25 14:16:02 -0700

    Bugfixes

--- email_setup.sh
+++ email_setup.sh
@@ -4,5 +4,5 @@ PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin #environment
 mkdir /home/pi/.certs; #creates certificate directory
 certutil -N -f pwdfile.txt -d ~/.certs; #encrpyts the certificate database
 echo -n | openssl s_client -connect smtp.gmail.com:465 | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > ~/.certs/gmail.crt; #adds the certificate
-sudo cat /home/pi/dd_script/account_settings.txt >> /etc/nail.rc; #adds the account settings
-rm /home/pi/dd_script/account_settings.txt /home/pi/dd_script/certutil.txt; #cleans up
+sudo cat /home/pi/dd_script/account_settings.txt >> sudo /etc/nail.rc; #adds the account settings
+rm /home/pi/dd_script/account_settings.txt pwdfile.txt; #cleans up
--- timezone.sh
+++ timezone.sh
@@ -2,5 +2,5 @@
 #Sets the proper timezone
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin #environment
 TIMEZONE="America/Los_Angeles" #sets location as variable
-echo $TIMEZONE > /etc/timezone #sends variable to timezone folder
-cp /usr/share/zoneinfo/${TIMEZONE} /etc/localtime #copies location to localtime
+sudo echo $TIMEZONE > /etc/timezone #sends variable to timezone folder
+sudo cp /usr/share/zoneinfo/${TIMEZONE} /etc/localtime #copies location to localtime
